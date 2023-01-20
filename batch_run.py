4a245a915a4999e8d13778687e879bf6843fe636
Yang Xianglin <yxlyzxl@gmail.com>
2023-01-20 12:04:06 +0800

    minor

---
+++ batch_run.py
@@ -0,0 +1,20 @@
+import os
+import argparse
+from config import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--baseline', type=str, choices=BASELINES)
+args = parser.parse_args()
+BASELINE = args.baseline
+
+for N in Ns:
+    for DATASET in DATASETS:
+        for DISTRIBUTION in DISTRIBUTIONS:
+            if BASELINE == "MOTS":
+                if DISTRIBUTION in ["Gamma", "Poisson"]:
+                    continue
+            if BASELINE in ["TSGreedy", "ExpTS_plus"]:
+                for PROB in PROBS_fn(N):
+                    os.system("python main.py --baseline {} -k {} --dataset {} --distribution {} --repeat {} --period {} --prob {}".format(BASELINE, N, DATASET, DISTRIBUTION, REPEAT, PERIOD, PROB))
+            else:
+                os.system("python main.py --baseline {} -k {} --dataset {} --distribution {} --repeat {} --period {}".format(BASELINE, N, DATASET, DISTRIBUTION, REPEAT, PERIOD))
--- config.py
+++ config.py
@@ -1,10 +1,27 @@
 import numpy as np
+# # Define hyperparameters
+# Ns = [10,20,50]
+# DATASETS = ["Uniform", "TruncatedGaussian", "Segment"]
+# DISTRIBUTIONS = ["Gaussian", "Bernoulli", "Poisson", "Gamma"]
+# BASELINES = ["TS", "TSGreedy","KL_UCB",  "KL_UCB_plus_plus", "MOTS", "ExpTS", "ExpTS_plus"]
+
+# # [[1/N, 1/np.sqrt(N), 1/np.log(N)] for N in Ns]
+# PROBS_fn = lambda n: [1/n, 1/np.sqrt(n), 1/np.log(n)]
+
+
+# T = pow(10, 5)
+# REPEAT = 3
+# PERIOD = 50
+
 # Define hyperparameters
 Ns = [10,20,50]
 DATASETS = ["Uniform", "TruncatedGaussian", "Segment"]
 DISTRIBUTIONS = ["Gaussian", "Bernoulli", "Poisson", "Gamma"]
-BASELINES = ["TS", "TSGreedy","KL_UCB",  "KL_UCB_plus_plus", "MOTS", "ExpTS", "ExpTS_plus"]
-PROBS = [[1/N, 1/np.sqrt(N), 1/np.log(N)] for N in Ns]
-T = pow(10, 6)
-REPEAT = 1000
+BASELINES = ["TS", "TSGreedy", "MOTS"]
+
+# [[1/N, 1/np.sqrt(N), 1/np.log(N)] for N in Ns]
+PROBS_fn = lambda n: [1/n, 1/np.sqrt(n), 1/np.log(n)]
+
+T = pow(10, 5)
+REPEAT = 50
 PERIOD = 500
