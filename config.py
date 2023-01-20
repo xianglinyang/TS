0d363f17de20f1ef0760504888c07b3455954b2f
Yang Xianglin <yxlyzxl@gmail.com>
2023-01-21 00:08:12 +0800

    update hyperparameters

--- config.py
+++ config.py
@@ -1,27 +1,19 @@
 import numpy as np
-# # Define hyperparameters
-# Ns = [10,20,50]
-# DATASETS = ["Uniform", "TruncatedGaussian", "Segment"]
-# DISTRIBUTIONS = ["Gaussian", "Bernoulli", "Poisson", "Gamma"]
-# BASELINES = ["TS", "TSGreedy","KL_UCB",  "KL_UCB_plus_plus", "MOTS", "ExpTS", "ExpTS_plus"]
-
-# # [[1/N, 1/np.sqrt(N), 1/np.log(N)] for N in Ns]
-# PROBS_fn = lambda n: [1/n, 1/np.sqrt(n), 1/np.log(n)]
-
-
-# T = pow(10, 5)
-# REPEAT = 3
-# PERIOD = 50
 
 # Define hyperparameters
-Ns = [10,20,50]
-DATASETS = ["Uniform", "TruncatedGaussian", "Segment"]
+Ns = [10, 50]
 DISTRIBUTIONS = ["Gaussian", "Bernoulli", "Poisson", "Gamma"]
-BASELINES = ["TS", "TSGreedy", "MOTS"]
+BASELINES = ["TS", "TSGreedy","KL_UCB",  "KL_UCB_plus_plus", "MOTS", "ExpTS", "ExpTS_plus"]
+PROBS_fn = lambda n: [1/n, 1/np.sqrt(n)]
+T = pow(10, 4)
+PERIOD = 100
 
-# [[1/N, 1/np.sqrt(N), 1/np.log(N)] for N in Ns]
-PROBS_fn = lambda n: [1/n, 1/np.sqrt(n), 1/np.log(n)]
-
-T = pow(10, 5)
-REPEAT = 50
-PERIOD = 500
+REPEAT_T = {
+    "TS": 1000,
+    "TSGreedy": 1000,
+    "MOTS": 1000,
+    "KL_UCB": 200,
+    "KL_UCB_plus_plus": 200,
+    "ExpTS": 50,
+    "ExpTS_plus": 50
+}
