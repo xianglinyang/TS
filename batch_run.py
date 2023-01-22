1846f7706670f9707179db22f5cefa341d0df217
Yang Xianglin <yxlyzxl@gmail.com>
2023-01-22 13:38:16 +0800

    update

--- batch_run.py
+++ batch_run.py
@@ -8,13 +8,12 @@ args = parser.parse_args()
 BASELINE = args.baseline
 
 for N in Ns:
-    for DATASET in DATASETS:
-        for DISTRIBUTION in DISTRIBUTIONS:
-            if BASELINE == "MOTS":
-                if DISTRIBUTION in ["Gamma", "Poisson"]:
-                    continue
-            if BASELINE in ["TSGreedy", "ExpTS_plus"]:
-                for PROB in PROBS_fn(N):
-                    os.system("python main.py --baseline {} -k {} --distribution {} --repeat {} --period {} --prob {}".format(BASELINE, N, DATASET, DISTRIBUTION, REPEAT, PERIOD, PROB))
-            else:
-                os.system("python main.py --baseline {} -k {} --distribution {} --repeat {} --period {}".format(BASELINE, N, DATASET, DISTRIBUTION, REPEAT, PERIOD))
+    for DISTRIBUTION in DISTRIBUTIONS:
+        if BASELINE == "MOTS":
+            if DISTRIBUTION in ["Gamma", "Poisson"]:
+                continue
+        if BASELINE in ["TSGreedy", "ExpTS_plus"]:
+            for PROB in PROBS_fn(N):
+                os.system("python main.py --baseline {} -k {} --distribution {} --repeat {} --period {} --prob {}".format(BASELINE, N, DATASET, DISTRIBUTION, REPEAT, PERIOD, PROB))
+        else:
+            os.system("python main.py --baseline {} -k {} --distribution {} --repeat {} --period {}".format(BASELINE, N, DATASET, DISTRIBUTION, REPEAT, PERIOD))
